a47b8432c2f0524206693c5b8a37f8f8d5c8ae2b
AnnoyedNormad <nikita.pimenov.2016@gmail.com>
2023-12-31 11:47:05 +0300

    logger was initialized

--- cmd/ticket-office/main.go
+++ cmd/ticket-office/main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"log/slog"
+	"os"
 	"ticket-office/internal/config"
+)
+
+const (
+	devLogLevel   = "dev"
+	localLogLevel = "local"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	_ = cfg
+	log := NewLog(cfg.LogLevel)
+	log.Info("logger was initialized")
+}
+
+func NewLog(logLevel string) *slog.Logger {
+	var log *slog.Logger
+
+	switch logLevel {
+	case devLogLevel:
+		log = slog.New(slog.NewJSONHandler(os.Stdin, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	case localLogLevel:
+		log = slog.New(slog.NewTextHandler(os.Stdin, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	default:
+		log = slog.New(slog.NewTextHandler(os.Stdin, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
