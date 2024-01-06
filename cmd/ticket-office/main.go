165a0a09930b8fc0b88489856e8ff6a7ca8de622
AnnoyedNormad <nikita.pimenov.2016@gmail.com>
2024-01-06 21:31:14 +0300

    fix

--- cmd/ticket-office/main.go
+++ cmd/ticket-office/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"log/slog"
-	"net/http"
 	"os"
 	"ticket-office/internal/config"
-	"ticket-office/internal/http-server/handlers/event/saveevent"
 	"ticket-office/internal/storage/sqlite"
 )
 
@@ -28,31 +24,6 @@ func main() {
 	}
 
 	_ = storage
-
-	e := echo.New()
-
-	e.Use(middleware.Recover(), middleware.Logger())
-
-	eg := e.Group("/event")
-
-	eg.Use(middleware.BasicAuth(func(user, pass string, e echo.Context) (bool, error) {
-		if user == "home" && pass == "home" {
-			return true, nil
-		}
-		return false, nil
-	}))
-	eg.POST("/save", saveevent.New(storage))
-
-	//e.POST("/", saveuserstickets.New(storage))
-
-	server := http.Server{
-		Addr:         cfg.Addr,
-		Handler:      e,
-		ReadTimeout:  cfg.ReadTimeout,
-		WriteTimeout: cfg.WriteTimeout,
-	}
-
-	server.ListenAndServe()
 }
 
 func NewLog(logLevel string) *slog.Logger {
